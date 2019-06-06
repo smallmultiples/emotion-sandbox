48ef45522b8ab278fc5c6197c401d5c156c08292
Joachim Bachstatter <joachim.bachstatter@gmail.com>
2019-06-06 16:22:25 +1000

    Support media queries

--- src/createSandboxPlugin.js
+++ src/createSandboxPlugin.js
@@ -1,6 +1,6 @@
 function createSandboxPlugin(selector) {
   return (context, content, selectors, parents, line, column, length, type) => {
-    if (context !== 2 || type === 107) return;
+    if (context !== 2 || type === 107 || type === 109) return;
 
     for (let i = 0; i < selectors.length; i++) {
       selectors[i] = `${selector} ${selectors[i]}`;
