a16f847154cf564b532246d92160f1b2ae7d2fd3
Joachim Bachstatter <joachim.bachstatter@gmail.com>
2019-06-06 13:50:11 +1000

    The selectors was added in wrong order. Taking some inpirations from: https://github.com/Andarist/stylis-plugin-extra-scope/blob/master/src/index.js

--- src/createSandboxPlugin.js
+++ src/createSandboxPlugin.js
@@ -1,13 +1,10 @@
 function createSandboxPlugin(selector) {
-  return (context, content) => {
-    if (context === -2) {
-      const newContent = content
-        .replace(/,/g, `, ${selector} `)
-        .replace(/}/g, `} ${selector} `);
-      return `${selector} ${newContent}`;
-    }
+  return (context, content, selectors, parents, line, column, length, type) => {
+    if (context !== 2 || type === 107) return;
 
-    return content;
+    for (let i = 0; i < selectors.length; i++) {
+      selectors[i] = `${selector}${selectors[i]}`;
+    }
   };
 }
 
